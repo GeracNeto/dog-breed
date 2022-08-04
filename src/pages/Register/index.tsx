e4edd5366977c24bd0f8ef0c17fdb983c26fcb7a
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-04 11:41:24 -0300

    Atualizado local da pasta components

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -4,8 +4,8 @@
 import './style.css';
 
 // Componentes
-import { Button } from "../components/Button";
-import { Input } from "../components/Input";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 const Register = () => {
 
