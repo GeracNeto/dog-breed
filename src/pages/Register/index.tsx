7f01203c211235c5f83e78c6b60fd34ed8c34947
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-04 10:20:45 -0300

    Importado componentes Input e Button na page Resgister

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,9 +1,18 @@
 // Página de registro
 
+// Componentes
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
 const Register = () => {
 
     return (
         <div className="register">
+          <Input 
+            name="email"
+            type='email'
+          />
+          <Button>Login</Button>
 
         </div>
       );
