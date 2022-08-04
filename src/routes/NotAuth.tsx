44f3e9522604aebf34078e20200b571c4373b91f
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-04 14:48:14 -0300

    Atualizado NotAuth para retornar apenas a page register

--- src/routes/NotAuth.tsx
+++ src/routes/NotAuth.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import { Register } from "../pages/Register";
 
 export function NotAuth(){
 
     return(
-        <Routes location='/register'>
-            <Route path='register' element={<Register />} />
-        </Routes>
+        <Register />
     );
 };
