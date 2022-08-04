58c0cffc41339bbbba868a540d3fb049790280d3
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-04 16:24:19 -0300

    Atualizado App
    Criada função para fazer o POST com o email digitado pelo usuário, criada função para fazer o GET da list tendo como argumento o token recebido pela chamada POST

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,49 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Chihuahua } from './pages/Chihuahua';
+import { List } from './pages/List';
+import { Register } from './pages/Register';
+import { api } from './service/api';
 
-import { Auth } from './routes/Auth';
-import { NotAuth } from './routes/NotAuth';
 
 import './styles/global.css';
 
 
 function App() {
 
-  const [test, setTest] = useState<boolean>(false)
+  const [userToken, setUserToken] = useState<string>(); // Variável para armazenar o token do usuário registrado
 
+    // Função que faz o método POST para postar o email do usuário na API
+  const postEmail = async(email: string) => {
+
+    await api.post('register', {
+      email: email
+    }).then(response => {
+      setUserToken(response.data.user.token);
+      getList(response.data.user.token);
+      console.log(response.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  const getList = async (token: string) => {
+
+    await api.get('list', {
+      headers:{ Authorization: token}
+    }).then(response => {
+      console.log(response.data)
+    }).catch(error => {
+      console.log(error);
+    })
+  }
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<NotAuth />} />
-          <Route path='/list' element={test ? (<Auth />) : (<h1>404</h1>) } />
+          <Route path='/' element={<Register postEmail={postEmail}/>} />
+          <Route path='/list' element={<List />} />
           <Route path='/list/chihuahua' element={<Chihuahua />} />
         </Routes>
       </BrowserRouter>
