df4c91ccbbb4b7cb2405cdf55dd293413e64a4cb
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-04 14:50:53 -0300

    Atualizado App com as rotas /list e /list/chihuahua para testes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 // Styles
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Chihuahua } from './pages/Chihuahua';
+
+import { Auth } from './routes/Auth';
+import { NotAuth } from './routes/NotAuth';
+
 import './styles/global.css';
 
-// Components
-import { NotAuth } from './routes/NotAuth';
 
 function App() {
 
+  const [test, setTest] = useState<boolean>(false)
+
+
   return (
     <div className="App">
-      <NotAuth />
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<NotAuth />} />
+          <Route path='/list' element={test ? (<Auth />) : (<h1>404</h1>) } />
+          <Route path='/list/chihuahua' element={<Chihuahua />} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   );
