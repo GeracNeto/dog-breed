9cf948ce33d079a5f20f81d933fad6bae91e1fc1
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-05 12:50:19 -0300

    Criado Footer e Header da página

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+// Styles
+import './style.css';
+
+const Footer = () => {
+
+    return(
+        <footer>
+            <p>Powered by: Geraldo Pereira da Costa Neto</p>
+            <a href="https://www.linkedin.com/in/geraldo-costa-5568a8152/" target='_blank'> img linkedin here</a>
+        </footer>
+    );
+};
+
+export {Footer};
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+// Styles
+import './style.css';
+
+const Header = () => {
+
+    return(
+        <header>
+            <h1><a href="https://gitlab.com/q9-tecnologia/desafios/mobile/desafio-dogbreed" target='_blank'>Desafio Dog Breed</a></h1>
+        </header>
+    );
+};
+
+export {Header};
