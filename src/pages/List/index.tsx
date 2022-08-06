0faa3bcd52007feb679480a86a90b7d57d7494fe
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-05 22:45:28 -0300

    Criado useEffect com método get para pegar a lista de fotos das raças ao clicar no nomes das raças

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,21 +1,54 @@
+import { ReactNode, useContext, useEffect, useState } from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { api } from "../../service/api";
 import { Chihuahua } from "../Chihuahua";
 
 // Styles
 import './style.css';
 
+type DogProps = {
+    breed: string;
+    list: string[];
+}
+
 const List = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const [breed, setBreed] = useState<string>('');
+    const [dogs, setDogs] = useState<string[]>([]);
+
+    useEffect(() => {
+        // Retorna a lista das raças
+         api.get<DogProps>('list', {
+             headers:{ Authorization: user.token},
+             params: {breed: breed}
+           }).then(response => {
+             console.log(response.data);
+             setDogs(response.data.list);
+           })
+    }, [breed])
 
     return(
         <div className="list">
             <ul>
-                <Link to='chihuahua'><li>Chihuahua</li></Link>
-                <Link to='Husky'><li>Husky</li></Link>
-                <Link to='Labrador'><li>Labrador</li></Link>
-                <Link to='Pug'><li>Pug</li></Link>
+                <li onClick={() => setBreed('chihuahua')}>Chihuahua</li>
+                <li onClick={() => setBreed('husky')}>Husky</li>
+                <li onClick={() => setBreed('labrador')}>Labrador</li>
+                <li onClick={() => setBreed('pug')}>Pug</li>
+                
             </ul>
+            {dogs.map((dog) => (<img src={dog} alt={breed} key={dog}/>))}          
         </div>
     );
 };
 
 export {List};
+
+/*
+                <Link to='chihuahua'><li>Chihuahua</li></Link>
+                <Link to='husky' onClick={() => setBreed('husky')}><li>Husky</li></Link>
+                <Link to='labrador'><li>Labrador</li></Link>
+                <Link to='pug'><li>Pug</li></Link>
+*/
