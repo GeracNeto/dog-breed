2ae05fa0cdf1534a1efeb585d22ddf10b245e926
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-05 22:43:47 -0300

    Criado AuthContext
    Criada função de signIn para criação do usuário, criado provider para fornexer os dados do usuário recebios da API

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, ReactNode, useEffect, useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../service/api";
+
+type UserProps = {
+    _id: string;
+    email: string;
+    token: string;
+}
+
+type AuthProps = {
+    user: UserProps
+}
+
+type AuthContextData = {
+    signIn: (email: string) => Promise<void>;
+    user: UserProps;
+}
+
+type AuthProviderProps = {
+    children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+
+   const [user, setUser] = useState<UserProps>({} as UserProps);
+
+   const signIn = async(email: string) => {
+
+        try {
+            const response = await api.post<AuthProps>('register', {
+                email: email
+            })
+            const {user} = response.data;
+            console.log('Usuário criado:');
+            console.log(user);
+            setUser(user);
+            // setUser(response.data.user)
+        } catch (error) {
+            console.log(error);
+        }
+   }
+
+   const value = useMemo(() => ({
+    user,
+    signIn
+   }), [user])
+
+    return(
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
