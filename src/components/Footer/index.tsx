6d546bd2797cf07a4b967e832cd04f31f623df9e
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-07 13:25:23 -0300

    Adicionada imagem do linkedin e github para o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,17 @@
 // Styles
 import './style.css';
 
+// Images
+import Linkedin from "../../assets/images/linkedin.png";
+import Github from "../../assets/images/github.png";
+
 const Footer = () => {
 
     return(
         <footer>
             <p>Powered by: Geraldo Pereira da Costa Neto</p>
-            <a href="https://www.linkedin.com/in/geraldo-costa-5568a8152/" target='_blank'> img linkedin here</a>
+            <a href="https://www.linkedin.com/in/geraldo-costa-5568a8152/" target='_blank'><img src={Linkedin} alt="Linkedin-img" /></a>
+            <a href="https://github.com/GeracNeto" target='_blank'><img src={Github} alt="Github-img" /></a>
         </footer>
     );
 };
